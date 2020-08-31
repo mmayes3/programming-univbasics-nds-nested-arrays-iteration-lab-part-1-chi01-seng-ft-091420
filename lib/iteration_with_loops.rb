cf107123b207d295462200251a1f0f822e038428
classy-tier-0049 <mauricelynell@gmail.com>
2020-08-31 04:02:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,8 +1,17 @@
 def find_even_values(src)
   count = 0 
   
-  while count < find_even_values.count do 
+  while count < find_even_values.length do 
     count2 = 0
-    while count2 < coun
-
+    while count2 < find_even_values[count].length do
+      find_even_values[count][count2]
+      count2 += 1
+      if find_even_values[count][count2] % 2 == 0
+        p find_even_values[count][count2]
+      end
+    end
+    count += 1
+  end
 end
+
+find_even_values()
